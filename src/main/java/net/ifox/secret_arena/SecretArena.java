61b9021773a34ed89efaf74907854f7677b82edd
ifox <03613foxy@mail.ru>
2025-10-13 03:09:33 +0700

    Initial commit

---
+++ src/main/java/net/ifox/secret_arena/SecretArena.java
@@ -0,0 +1,15 @@
+package net.ifox.secret_arena;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SecretArena implements ModInitializer {
+	public static final String MOD_ID = "secret_arena";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/ifox/secret_arena/SecretArenaClient.java
@@ -0,0 +1,10 @@
+package net.ifox.secret_arena;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SecretArenaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
