289f0749d995ec0b50274885cee9b2e45a8c9c28
ifox <03613foxy@mail.ru>
2026-05-12 03:04:52 +0700

    i forgot to make everything flammable and used as fuel :wilted_rose:

--- src/main/java/net/ayfery/secret_arena/SecretArena.java
+++ src/main/java/net/ayfery/secret_arena/SecretArena.java
@@ -2,6 +2,7 @@ package net.ayfery.secret_arena;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.ayfery.secret_arena.block.SABlocks;
 import net.ayfery.secret_arena.item.SAItems;
@@ -26,5 +27,16 @@ public class SecretArena implements ModInitializer {
 		SABlocks.registerModBlocks();
 		SASounds.registerSounds();
 		FuelRegistry.INSTANCE.add(SABlocks.WOOD_CRATE, 2400);
+
+        FlammableBlockRegistry.getDefaultInstance().add(SABlocks.WOOD_CRATE,5, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(SABlocks.DRY_OAK_PLANKS,5, 20);
+        FlammableBlockRegistry.getDefaultInstance().add(SABlocks.DRY_OAK_STAIRS,5, 20);
+        FlammableBlockRegistry.getDefaultInstance().add(SABlocks.DRY_OAK_SLAB,5, 20);
+        FlammableBlockRegistry.getDefaultInstance().add(SABlocks.DRY_OAK_FENCE,5, 20);
+        FlammableBlockRegistry.getDefaultInstance().add(SABlocks.DRY_OAK_FENCE_GATE,5, 20);
+        FlammableBlockRegistry.getDefaultInstance().add(SABlocks.DRY_OAK_DOOR,5, 20);
+        FlammableBlockRegistry.getDefaultInstance().add(SABlocks.DRY_OAK_TRAPDOOR,5, 20);
+        FlammableBlockRegistry.getDefaultInstance().add(SABlocks.DRY_OAK_PRESSURE_PLATE,5, 20);
+        FlammableBlockRegistry.getDefaultInstance().add(SABlocks.DRY_OAK_BUTTON,5, 20);
 	}
 }
